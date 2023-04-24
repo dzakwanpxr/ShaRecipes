6391847fb3c87d4659b4356e62612d4120357067
dzakwanpxr <87738561+dzakwanpxr@users.noreply.github.com>
2023-04-24 15:49:43 +0700

    refactor: update card routing to navigate to full recipe page from 'see full recipe' link

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,7 +3,7 @@ import defaultFood from "../assets/food-bg.png";
 import { MdEdit, MdDelete } from "react-icons/md";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const DELETE_RECIPE = gql`
   mutation deleteRecipe($id: String!) {
@@ -15,12 +15,18 @@ const DELETE_RECIPE = gql`
 `;
 
 const Card = ({ item }) => {
+  const navigate = useNavigate();
   const [deleteRecipe] = useMutation(DELETE_RECIPE);
   const handleDelete = (e) => {
     e.preventDefault();
     if (confirm(`Are you sure you want to delete ${item.title}`) == true) {
       deleteRecipe({ variables: { id: item.id } });
     }
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    navigate(`/edit-recipe/${item.id}`);
   };
   return (
     // <div className="max-w-full bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg lg:max-w-sm">
@@ -37,35 +43,31 @@ const Card = ({ item }) => {
     // </div>
 
     <div className="w-full  bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg lg:max-w-sm">
-      <Link to={item.id}>
-        <img
-          className="rounded-t-lg object-cover w-full h-60"
-          src={item.image ? item.image : defaultFood}
-          alt="food-bg"
-        />
+      <img
+        className="rounded-t-lg object-cover w-full h-60"
+        src={item.image ? item.image : defaultFood}
+        alt="food-bg"
+      />
 
-        <div className="p-5 flex justify-between">
-          <div className="card-text">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 me-auto">
-              {item.title}
-            </h5>
-            <Link to={item.id}>
-              <p className="text-xs underline">See Full Recipe</p>
-            </Link>
-          </div>
-          <div className="flex card-icon">
-            <Link to={`/edit-recipe/${item.id}`}>
-              <MdEdit size={24} />
-            </Link>
-            <MdDelete
-              color="red"
-              size={24}
-              className="cursor-pointer"
-              onClick={handleDelete}
-            />
-          </div>
+      <div className="p-5 flex justify-between">
+        <div className="card-text">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 me-auto">
+            {item.title}
+          </h5>
+          <Link to={item.id}>
+            <p className="text-xs underline">See Full Recipe</p>
+          </Link>
         </div>
-      </Link>
+        <div className="flex card-icon">
+          <MdEdit size={24} onClick={handleUpdate} />
+          <MdDelete
+            color="red"
+            size={24}
+            className="cursor-pointer"
+            onClick={handleDelete}
+          />
+        </div>
+      </div>
     </div>
   );
 };
